c539d36472c0b19a9fffdec3c083545d8b6a1aec
Max Stepanov <mu5cool@yandex.ru>
2020-01-26 17:02:52 +0300

    Commit-1

---
+++ src/main/java/com/stepanov/entity/EstimateDetails.java
@@ -0,0 +1,12 @@
+package com.stepanov.entity;
+
+public class EstimateDetails {
+
+    private int number;
+    private String positionName;
+    private String unit;
+    private int quantity;
+    private int price;
+    private int cost;
+
+}
